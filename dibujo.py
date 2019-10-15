a15598171a874e950d94cf9b0ca49e19bf51838a
targ <crackiman@gmail.com>
2019-10-14 19:32:44 -0500

    first commit

---
+++ dibujo.py
@@ -0,0 +1,18 @@
+from turtle import *
+t = Pen()
+def mystar(size, filled):
+    if filled == True:
+        t.begin_fill()
+    for x in range(1, 19):
+        t.forward(size)
+        if x % 2 == 0:
+            t.left(175)
+        else:
+            t.left(225)
+    if filled==True:
+        t.end_fill()
+t.color(0.9,0.75, 0)
+mystar(100, True)
+t.color(0,0,0)
+mystar(100, False)
+    
